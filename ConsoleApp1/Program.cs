bf7308215b32c4b303436e0ceb5cc4f4dbd1a489
John Doe <john.doe@gmail.com>
2019-07-10 10:22:24 -0300

    exemplos date time

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,8 +11,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string[] nameList = new string[2] { "valor 1", "valor 2" };
 
-            Console.WriteLine("Ola Git");
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime fisrTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("dd:MM:yyyy--hh:mm:ss:f.fff");
+                Console.WriteLine(dateList[i]);
+            }
+
+            Console.WriteLine((DateTime.Now - fisrTime).ToString());
+
+            Console.ReadKey();
+        
         }
     }
 }
