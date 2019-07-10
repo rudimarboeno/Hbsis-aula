4f1a353587fcf01af421666861fde2059fa0e22e
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:56 -0300

    Arrais de duas dimenções

---
+++ exemplo 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                  {{"O pequeno","sim"},{"O grande","não"} };
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+            {
+                /* i é a linha // 0 é a coluna
+                   i é a linha // 1 é a coluna*/
+                var Nomelivro  = listaDeLivros[i,0];
+                var disponivel = listaDeLivros[i,1];
+
+                Console.WriteLine($"Nome do livro:{Nomelivro} disponivel:{disponivel}.");
+ 
+            }
+               Console.ReadKey();
+        }
+    }
+}
