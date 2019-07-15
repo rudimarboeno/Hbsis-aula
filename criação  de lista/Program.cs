ca0c3ade016b26acdfac93f8714a267478c921dc
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 08:41:50 -0300

    Lista de string

---
+++ criação  de lista/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criação__de_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Iniciando uma lista de string com array linear
+            string[] lista = new string[10];//arui é definimos as posições de lista iniciando ela com 10
+            //espaços na memoria para alocar informações de texto
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                //carregando os valores da nossa lista aqui
+                //aonde "i" representa cada espaço que temos disponivel
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+                //DateTime é um tipo do .net c# que disponibiliza funções relacionadas a datas
+                //e horas, ou seja se precisar trabalhar com essas informações é possivel usar esse cara.
+            }
+            //laço de petição que usamos para varrer nossa lista de maneira mais simples
+            foreach (var item in lista)//"var intem" indica uma unidade da nossa lista "in lista"
+                //indica a lista que desejamos varrer
+                Console.WriteLine(item);//Aqui apresentamos essa informação na tela
+
+            Console.ReadKey();
+        }
+    }
+}
