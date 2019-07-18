bce73cbf5b662ebc2f0e563e4500d86c1fc31b75
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:08:22 -0300

    SQL Comads food truck

---
+++ Sql/SQLQuery1- SELECT DO FOOD TRUCK.sql
@@ -0,0 +1,11 @@
+﻿select --chama as tabelas que quero pesquisar
+		Dogs,               --nome que quero chamar para coluna
+		SUM(TotalVendas) as 'Total Vendas',
+		MONTH(DataVenda) as 'Mês'
+from FoodTruckZezinho -- chamo o nome da tabela
+where MONTH(DataVenda) BETWEEN 1 and 4 --BETWEEN inicio mes 1 ate mes 4 vai contar apenas o que esta entre eles
+group by Dogs,MONTH(DataVenda)
+order by 'Total Vendas' desc;
+
+--SUM significa para somar agrupador
+--MONTH agrupa
---
+++ Sql/SQLQuery9-FoodTruck.sql
@@ -0,0 +1,12 @@
+﻿select 'MediaVenda' as 'MediaVenda',
+		SUM(Temp.Media) as 'Vendas',
+		Temp.MesVenda
+from (select 
+			DogCasa,
+			DogEgg,
+			DogSoya
+		(SUM(TotalVendas) / COUNT(*)) as 'Medias',
+		YEAR(DataVenda) as 'MesVenda'
+from FoodTruckZezinho
+group by TotalVendas,YEAR(DataVenda)) Temp
+group by Temp.MesVenda;
