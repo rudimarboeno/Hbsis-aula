7f0c603567612f9d10b9c6093674377be3d97b36
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 17:26:32 -0300

    SistemaBoletimEscolaBD

---
+++ Sql/SQLQuery6-Consulta no banco.sql
@@ -0,0 +1,8 @@
+﻿select 
+		BL.Nome,
+		COUNT(LV.Alocado) as 'QuantidadedeLivros',
+		US.Nome as 'UserName'
+from Biblioteca BL
+inner join Livros LV on BL.Id = LV.Biblioteca
+inner join Usuario US on LV.UsuInc = US.Id
+group by BL.Nome,US.Nome
