39704c656b69e59fbf8957e026199e174e2708bb
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 17:30:03 -0300

    carro4x4

---
+++ MeuCarro4x4Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using MeuCarro4x4Class.MyFirstClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCarro4x4Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = new Carro();
+
+            teste.Rodas = 5;
+
+            teste.janela = 5;
+
+            teste.MostrarCarro();
+
+            Console.ReadKey();
+        }
+    }
+}
