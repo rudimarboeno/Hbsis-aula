1f02b752acb0e7d9bd3a790945c5bb7aaf5fcf43
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-09 14:16:19 -0300

    WpfAppEntity

---
+++ WpfAppEntity/WpfAppEntity/Data/Autore.cs
@@ -0,0 +1,20 @@
+namespace WpfAppEntity.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Autore
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(1000)]
+        public string Descricao { get; set; }
+    }
+}
---
+++ WpfAppEntity/WpfAppEntity/Views/usGridView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppEntity.Data;
+
+namespace WpfAppEntity.Views
+{
+    /// <summary>
+    /// Interaction logic for usGridView.xaml
+    /// </summary>
+    public partial class usGridView : UserControl
+    {
+        public usGridView()
+        {
+            InitializeComponent();
+        }
+
+        public BibliotecaDBContext context = new BibliotecaDBContext();
+
+        private void DtGridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            dtGridView.ItemsSource = context.Usuarios.ToList<Usuario>();
+        }
+
+        private void DtGridView_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            var objectEdit = ((DataGrid)sender).SelectedValue as Usuario;
+
+            
+            
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+
+            MessageBox.Show("Carregando...");
+            MessageBox.Show("Atualizado com sucesso!");
+
+        }
+    }
+}
